3d99874406392dee6d27644bf5bdda40aee1d2d4
Nabildwi940 <145630817+Nabildwi940@users.noreply.github.com>
2024-01-04 00:14:32 +0700

    perbaikan

--- index.php
+++ index.php
@@ -1,48 +1,139 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-xxx" crossorigin="anonymous" />
-    <link rel="stylesheet" href="styles.css">
-    <title>Penjualan Mobil Bekas</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Khalis Cars</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
 </head>
-<body>
-  <center>
-    <header>
-        <h1>Penjualan Mobil Bekas</h1>
-    </header>
-   
+<body class="hold-transition sidebar-mini">
+ 
+<!-- Site wrapper -->
+<div class="wrapper">
+  <!-- Navbar -->
+  <?php include_once('navbar.php'); ?>
+  <!-- /.navbar -->
+  <!-- Main Sidebar Container -->
+  <?php include_once('sidebar.php'); ?>
 
-    <section id="featured-listings">
-        <h2><a href="list_mobil.html">List Mobil</a></h2>
-        <!-- Tambahkan daftar mobil bekas unggulan dengan gambar dan informasi singkat -->
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Khalis Cars</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Khalis Cars</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
     </section>
+    <center>    
+    <!-- Main content -->
+    <section class="content">
+    <img src="dist/img/sampulmobil.png" class="img-fluid" alt="img" width="700">
+      <!-- tombol jenis mobil -->
+      <br>
+      <div class="container">
+<p class="d-inline-flex gap-1">
+<img src="dist/img/logomobil.png" alt="Icon" width="50">
+  <a href="jenismobil.php" class="btn btn-primary" role="button" data-bs-toggle="button">jenis mobil</a>
+  </p>
+  
+  <!-- tombol pembeli -->
+<p class="d-inline-flex gap-1">
+<img src="dist/img/logomobil.png" alt="Icon" width="50">
+  <a href="pembeli.php" class="btn btn-primary" role="button" data-bs-toggle="button">Daftar Pembeli</a>
+  </p>
+  <br>
+  <!-- tombol jenis mobil -->
+<p class="d-inline-flex gap-1">
+<img src="dist/img/logomobil.png" alt="Icon" width="50">
+  <a href="jenismobil.php" class="btn btn-primary" role="button" data-bs-toggle="button">Pengecekan</a>
+  </p>
+  
+  <!-- tombol pembeli -->
+<p class="d-inline-flex gap-1">
+<img src="dist/img/logomobil.png" alt="Icon" width="50">
+  <a href="pembeli.php" class="btn btn-primary" role="button" data-bs-toggle="button">Transaksi</a>
+  </p>
+  <br>
+  <!-- tombol jenis mobil -->
+<p class="d-inline-flex gap-1">
+<img src="dist/img/logomobil.png" alt="Icon" width="50">
+  <a href="jenismobil.php" class="btn btn-primary" role="button" data-bs-toggle="button">penyerahan</a>
+  </p>
+  
+ 
+  
+  
+  <br>
+  <div class="card" style="width: 30rem;">
+  <div class="card-body">
+    <!-- tombol pembeli -->
+<p class="d-inline-flex gap-1">
+<img src="dist/img/logomobil.png" alt="Icon" width="50">
+  <a href="pembeli.php" class="btn btn-primary" role="button" data-bs-toggle="button">pembeli</a>
+  </p>
+  <!-- tombol jenis mobil -->
+<p class="d-inline-flex gap-1">
+<img src="dist/img/logomobil.png" alt="Icon" width="50">
+  <a href="jenismobil.php" class="btn btn-primary" role="button" data-bs-toggle="button">jenis mobil</a>
+  </p>
+  </div>
+  </div>
+</div>
+          <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse" >
+              <i class="fas fa-minus"></i>
+            </button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+              <i class="fas fa-times"></i>
+            </button>
+          </div>
+        </div>
+       
+        <!-- /.card-body -->
+        <div class="card-footer">
+          Footer
+        </div>
+        <!-- /.card-footer-->
+      </div>
+      <!-- /.card -->
 
-    <section id="categories">
-    <h2><a href="penjualan.html">Penjualan</a></h2>
-        <!-- Tambahkan kategori mobil bekas -->
-        <!-- Contoh: Sedan, SUV, Hatchback, dll. -->
     </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  
+  <?php include_once('footer.php')?>
 
-    <section id="latest-listings">
-    <h2><a href="mobil_terjual.html">Mobil Terjual</a></h2>
-        <!-- Tambahkan daftar mobil bekas terbaru dengan gambar dan informasi singkat -->
-    </section>
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
 
-    <section id="featured-listings">
-    <h2><a href="profil_penjual.html">Profil Penjual</a></h2>
-        <!-- Tambahkan daftar mobil bekas unggulan dengan gambar dan informasi singkat -->
-    </section>
-
-    <section id="featured-listings">
-    <h2><a href="alamat.html">Alamat</a></h2>
-        <!-- Tambahkan daftar mobil bekas unggulan dengan gambar dan informasi singkat -->
-    </section>
-    
-    <footer>
-        <p>&copy; 2023 Penjualan Mobil Bekas</p>
-    </footer>
-    </center>
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+</center>
 </body>
 </html>
--- sidebar.php
+++ sidebar.php
@@ -1,22 +1,15 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="#" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
-    </a>
+   
 
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="dist/img/logomobil.png" class="img-circle elevation-2">
         </div>
-        <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
-        </div>
-      </div>
-
+        <br>
+     
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
@@ -28,22 +21,18 @@
                 Dashboard
               </p>
             </a>
-
+            
           <li class="nav-item">
-          <a href="#" class="nav-link">
+          <img src="dist/img/logomobil.png">
+          <a href="jenismobil.php" class="nav-link">
               <i class="nav-icon fas fa-solid fa-users"></i>
               <p>
-                Mahasiswa
+                Jenis Mobil
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="../../index.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v1</p>
-                </a>
-              </li>
+
+            
               <li class="nav-item">
                 <a href="../../index2.html" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
